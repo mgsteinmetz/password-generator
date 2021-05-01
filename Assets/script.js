4e13b1a5a622ead1b3c39770affd7636b3b581af
Mike Steinmetz <mikesteinmetz00@gmail.com>
2021-05-01 15:43:54 -0500

    adding password criteria

--- Assets/script.js
+++ Assets/script.js
@@ -1,14 +1,37 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
+  
+  // Adding random lowercase, uppecase, number, and special character functions
+  function getRandomLower() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+  }
+  function getRandomUpper() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+  }
+  function getRandomNumber() {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+  }
+  function getRandomCharacter() {
+    const symbols = '!@#$%^&(*/)?[.~]';
+    return symbols[Math.floor(Math.random() * symbols.length)];
+  }
 }
+
+// setting DOM elements
+const randomFunct = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  symbol: getRandomCharacter
+};
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
